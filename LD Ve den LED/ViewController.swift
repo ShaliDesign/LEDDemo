eb69c60b4a4b5356d643b0fe62c656e3d0bed1b7
Linh&ShawnD <linhshawn-d@LinhShawnDs-MacBook-Pro.local>
2017-02-10 19:23:33 -0800

    Ve den LED/ Chua connect dc textfield & button

--- LD Ve den LED/ViewController.swift
+++ LD Ve den LED/ViewController.swift
@@ -10,16 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    var thanhvien = ["ong", "ba", "bo", "me"]
+    
+    var n = 5
+    
+    
+    var _margin: CGFloat = 60
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        
+    }
+    
+    @IBOutlet weak var LEDTextField: UITextField!
+    
+    @IBAction func Draw(_ sender: Any) {
+        
+        DrawRowOfBall()
+        
+    }
+        
+    func DrawRowOfBall()
+    
+    {
+        for indexHang in 0...n {
+            
+            
+            for indexCot in 0...n {
+            
+            print(index)
+        
+        let image = UIImage(named: "green")
+        let ball = UIImageView(image: image)
+       
+            ball.center = CGPoint(x: _margin + CGFloat(indexHang) * SpaceBetweenBallX(),y: CGFloat(indexCot)*SpaceBetweenBallY() + _margin)
+                
+        self.view.addSubview(ball)
+        
+          }
+       }
+    }
+    
+    func SpaceBetweenBallY() -> CGFloat {
+        let space = (self.view.bounds.size.height - 2*_margin)/CGFloat(n-1)
+        return space
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func SpaceBetweenBallX() -> CGFloat {
+        let space = (self.view.bounds.size.width - 2*_margin)/CGFloat(n-1)
+        return space
     }
-
-
+    
+    
+    
+    
+    
 }
-
